6963e0234bc6402d3cb65d06fcef657aa486fbee
Ashen Thilakarathna <134621843+Ashein00@users.noreply.github.com>
2024-11-04 00:35:19 +0530

    jsons to json :)

--- controller/userController.js
+++ controller/userController.js
@@ -2,42 +2,42 @@
 // POST : api/users/register
 // UNPROTECTED
 const registerUser = (req, res, next) => {
-  res.jsons("Register User");
+  res.json("Register User");
 };
 
 // ==================== LOGIN A REGISTERED USER
 // POST : api/users/login
 // UNPROTECTED
 const loginUser = (req, res, next) => {
-  res.jsons("Login User");
+  res.json("Login User");
 };
 
 // ==================== USER PROFILE
 // POST : api/users/:id
 // UNPROTECTED
 const getUser = (req, res, next) => {
-  res.jsons("User Profile");
+  res.json("User Profile");
 };
 
 // ==================== CHANGE USER AVATAR
 // POST : api/users/change-avatar
 // UNPROTECTED
 const changeAvatar = (req, res, next) => {
-  res.jsons("Change user avatar");
+  res.json("Change user avatar");
 };
 
 // ==================== EDIT USER DETAILS (from profile)
 // POST : api/users/edit-user
 // UNPROTECTED
 const editUser = (req, res, next) => {
-  res.jsons("Edit user details");
+  res.json("Edit user details");
 };
 
 // ==================== GET AUTHORS
 // POST : api/users/authors
 // UNPROTECTED
 const getAuthors = (req, res, next) => {
-  res.jsons("Get all users/authors");
+  res.json("Get all users/authors");
 };
 
 module.exports = {
