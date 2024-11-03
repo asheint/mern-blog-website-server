fc6a6e31d5d8a8d56123e08e6a71e3ad263ac406
Ashen Thilakarathna <134621843+Ashein00@users.noreply.github.com>
2024-11-03 22:36:49 +0530

    userRoutes and postRoutes added

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const cors = require("cors");
-const connect = require("mongoose");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
-const app = express();
+const userRoutes = require("./routes/userRoutes");
+const postRoutes = require("./routes/postRoutes");
 
-app.listen(5000, () => console.log("Server is running on port 5000"));
+const app = express();
+const port = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+
+mongoose
+  .connect(MONGO_URI)
+  .then(
+    app.listen(5000, () => console.log(`Server is running on port ${port}`))
+  )
+  .catch((error) => {
+    console.log(error);
+  });
