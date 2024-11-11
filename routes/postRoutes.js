147b27490bf1d9238ecdec7924986ccc8aefaa9d
Ashen Thilakarathna <134621843+Ashein00@users.noreply.github.com>
2024-11-11 14:37:03 +0530

    Post model created

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -10,14 +10,16 @@ const {
   deletePost,
 } = require("../controller/postControllers");
 
+const { authMiddleware } = require("../middleware/authMiddleware");
+
 const router = Router();
 
-router.post("/", createPost);
+router.post("/", authMiddleware, createPost);
 router.get("/", getPosts);
 router.get("/:id", getPost);
 router.get("/categories/:category", getCatPosts);
 router.get("/users/:id", getUserPosts);
-router.patch("/:id", editPost);
-router.delete("/:id", deletePost);
+router.patch("/:id", authMiddleware, editPost);
+router.delete("/:id", authMiddleware, deletePost);
 
 module.exports = router;
